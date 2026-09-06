1c3325a65976e16bd9b1a74f69b9b66428d5a56d
karrisanthoshigayatri <karrisanthoshigayatri@gmail.com>
2026-09-07 03:54:55 +0530

    changes

--- backend/chk_pc.py
+++
@@ -1,6 +0,0 @@
-from app.db.session import engine
-from sqlalchemy import text, inspect as sa_inspect
-with engine.connect() as conn:
-    cols = sa_inspect(conn).get_columns("partner_capability", schema="public")
-    for c in cols:
-        print(c["name"], str(c["type"]))
